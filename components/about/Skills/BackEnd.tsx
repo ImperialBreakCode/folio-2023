ba6a9ed55e797a5be65b7445909f30ea639cdc52
ImperialBreakCode <eeeda522@gmail.com>
2023-08-19 17:22:37 +0000

    I forgot the databases

--- components/about/Skills/BackEnd.tsx
+++ components/about/Skills/BackEnd.tsx
@@ -5,6 +5,8 @@ import aspnetLogo from '@/public/about/backend/aspnet_logo.png';
 import flask from '@/public/about/backend/flask_logo.png';
 import sqlAlchemy from '@/public/about/backend/SQLAlchemy.svg.png';
 import efcoreLogo from '@/public/about/backend/efcore_logo.png';
+import postgresql from '@/public/about/backend/postgresql.png';
+import mssql from '@/public/about/backend/mssql-server.png';
 
 const Backend = () => {
 	return (
@@ -12,8 +14,11 @@ const Backend = () => {
 			<SkillBox name='.net' src={netLogo} />
 			<SkillBox name='asp.net' src={aspnetLogo} />
 			<SkillBox name='flask' src={flask} />
+			<SkillBox name='ms SQL' src={mssql} />
+			<SkillBox name='postgreSQL - basic experience' src={postgresql} />
 			<SkillBox name='entity framework' src={efcoreLogo} />
 			<SkillBox name='sql alchemy' src={sqlAlchemy} />
+
 		</>
 	);
 };
